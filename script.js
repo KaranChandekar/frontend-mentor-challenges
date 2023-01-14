56342b0f8cf7adab12631267760dfbea958753ea
Karan <karanchandekar431@gmail.com>
2023-01-14 12:40:09 +0530

    update folder structure

--- script.js
+++
@@ -1,8 +0,0 @@
-AOS.init();
-
-const toggleButton = document.getElementById("nav-toggle");
-const navLinks = document.getElementById("nav-links");
-
-toggleButton.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
