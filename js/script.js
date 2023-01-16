e036297274693009f387865955184947ef19c5f4
Karan <karanchandekar431@gmail.com>
2023-01-16 19:37:53 +0530

    big update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 AOS.init();
 
-const menuIcon = document.getElementById("menu-icon");
+const toggleButton = document.getElementById("nav-toggle");
 const navLinks = document.getElementById("nav-links");
 
-const onToggleMenu = (e) => {
-  e.name = e.name === "menu" ? "close" : "menu";
-
-  navLinks.classList.toggle("opacity-0");
-  navLinks.classList.toggle("top-[-400px]");
-};
+toggleButton.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
