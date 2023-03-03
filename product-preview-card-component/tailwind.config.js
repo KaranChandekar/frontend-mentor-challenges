5ee9bc746bd94cfd3bd2ad46dacd2fe02633c46b
Karan Chandekar <karanchandekar431@gmail.com>
2023-03-03 16:01:30 +0530

    feat: styled for mobile and desktop

--- product-preview-card-component/tailwind.config.js
+++ product-preview-card-component/tailwind.config.js
@@ -6,12 +6,16 @@ module.exports = {
       'dark-cyan': 'hsl(158, 36%, 37%)',
       cream: 'hsl(30, 38%, 92%)',
       'very-dark-blue': 'hsl(212, 21%, 14%)',
-      'dark-grayish-blue': 'hhsl(228, 12%, 48%)',
+      'dark-grayish-blue': 'hsl(228, 12%, 48%)',
       white: 'hsl(0, 0%, 100%)',
     },
     fontFamily: {
       fraunces: ['Fraunces', 'serif'],
       montserrat: ['Montserrat', 'sans - serif'],
+    },
+    backgroundImage: {
+      'product-mobile': "url('./images/image-product-mobile.jpg')",
+      'product-desktop': "url('./images/image-product-desktop.jpg')",
     },
   },
   plugins: [],
