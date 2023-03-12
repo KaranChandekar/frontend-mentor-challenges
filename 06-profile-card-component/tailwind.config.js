3c235645f77593585cf764fb0f47a76d86e3290a
Karan Chandekar <karanchandekar431@gmail.com>
2023-03-12 17:54:22 +0530

    feat: setup tailwind for profile card component

---
+++ 06-profile-card-component/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    colors: {
+      'dark-cyan': 'hsl(185, 75%, 39%)',
+      'dark-desaturated-blue': 'hsl(229, 23%, 23%)',
+      'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+      'dark-gray': 'hsl(0, 0%, 59%)',
+    },
+    fontFamily: {
+      sans: ['Kumbh Sans', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
